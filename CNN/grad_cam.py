891fc4e8c23f3e0362806f8e179d3de5abc9951a
janek <js116648@student.sgh.waw.pl>
2024-02-20 17:50:51 +0100

    cnn notebooks cleanup

--- CNN/grad_cam.py
+++ CNN/grad_cam.py
@@ -1,96 +1,3 @@
-# #Define Some Functions :
-# from skimage.io import imread, imsave
-#
-# last_conv_layer_name = model.layers[-12].name
-#
-# def get_img_array(img_path, size = (224 , 224)):
-#     img = tf.keras.utils.load_img(img_path, target_size=size)
-#     array = tf.keras.utils.img_to_array(img)
-#     array = np.expand_dims(array, axis=0)
-#     return array
-#
-#
-#
-# def make_gradcam_heatmap(img_array, model = model , last_conv_layer_name = last_conv_layer_name, pred_index=None):
-#     # First, we create a model that maps the input image to the activations
-#     # of the last conv layer as well as the output predictions
-#     grad_model = keras.models.Model(
-#         model.inputs, [model.get_layer(last_conv_layer_name).output, model.output]
-#     )
-#
-#     # Then, we compute the gradient of the top predicted class for our input image
-#     # with respect to the activations of the last conv layer
-#     with tf.GradientTape() as tape:
-#         last_conv_layer_output, preds = grad_model(img_array)
-#         if pred_index is None:
-#             pred_index = tf.argmax(preds[0])
-#         class_channel = preds[:, pred_index]
-#
-#     # This is the gradient of the output neuron (top predicted or chosen)
-#     # with regard to the output feature map of the last conv layer
-#     grads = tape.gradient(class_channel, last_conv_layer_output)
-#
-#     # This is a vector where each entry is the mean intensity of the gradient
-#     # over a specific feature map channel
-#     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-#
-#     # We multiply each channel in the feature map array
-#     # by "how important this channel is" with regard to the top predicted class
-#     # then sum all the channels to obtain the heatmap class activation
-#     last_conv_layer_output = last_conv_layer_output[0]
-#     heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
-#     heatmap = tf.squeeze(heatmap)
-#
-#     # For visualization purpose, we will also normalize the heatmap between 0 & 1
-#     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-#     return heatmap.numpy()
-#
-# def save_and_display_gradcam(img_path, heatmap, cam_path="cam.jpg", alpha=0.4 , view = False):
-#     # Load the original image
-#     img = tf.keras.utils.load_img(img_path)
-#     img = tf.keras.utils.img_to_array(img)
-#
-#     # Rescale heatmap to a range 0-255
-#     heatmap = np.uint8(255 * heatmap)
-#
-#     # Use jet colormap to colorize heatmap
-#     jet = mpl.colormaps["jet"]
-#
-#     # Use RGB values of the colormap
-#     jet_colors = jet(np.arange(256))[:, :3]
-#     jet_heatmap = jet_colors[heatmap]
-#
-#     # Create an image with RGB colorized heatmap
-#     jet_heatmap = tf.keras.utils.array_to_img(jet_heatmap)
-#     jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
-#     jet_heatmap = tf.keras.utils.img_to_array(jet_heatmap)
-#
-#     # Superimpose the heatmap on original image
-#     superimposed_img = jet_heatmap * alpha + img
-#     superimposed_img = tf.keras.utils.array_to_img(superimposed_img)
-#
-#     # Save the superimposed image
-#     superimposed_img.save(cam_path)
-#
-#     # Display Grad CAM
-#     if view :
-#         display(Image(cam_path))
-#
-# def decode_predictions(preds):
-#     classes = ['dyed-lifted-polyps', 'dyed-resection-margins', 'esophagitis',
-#        'normal-cecum', 'normal-pylorus', 'normal-z-line', 'polyps',
-#        'ulcerative-colitis']
-#     prediction = classes[np.argmax(preds)]
-#     return prediction
-#
-# def make_prediction (img_path , model = model , last_conv_layer_name = last_conv_layer_name , campath = "cam.jpeg" , view = False):
-#     img = get_img_array(img_path = img_path)
-#     img_array = get_img_array(img_path, size=(224 , 224))
-#     preds = model.predict(img_array)
-#     heatmap = make_gradcam_heatmap(img_array, model, last_conv_layer_name)
-#     save_and_display_gradcam(img_path, heatmap , cam_path=campath , view = view)
-#     return [campath , decode_predictions(preds)]
-
 import cv2
 import numpy as np
 from PIL import Image
@@ -189,6 +96,7 @@ def VizGradCAM(model, image, interpolant=0.5, plot_results=True):
     else:
         return cvt_heatmap
 
+
 def VizGradCAM_for_feature_map(model, img_array, interpolant=0.5, plot_results=False):
     last_conv_layer = next(x for x in model.layers[::-1] if isinstance(x, tf.keras.layers.Conv2D))
     grads_model = tf.keras.models.Model([model.inputs], [last_conv_layer.output, model.output])
@@ -206,7 +114,8 @@ def VizGradCAM_for_feature_map(model, img_array, interpolant=0.5, plot_results=F
     cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))
     cam = cv2.resize(cam, (img_array.shape[1], img_array.shape[0]))
     heatmap = np.uint8(255 * cam)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+    heatmap = cv2.applyColorMap(255 - heatmap, cv2.COLORMAP_JET)
 
     superimposed_img = heatmap * interpolant + img_array * (1 - interpolant)
     superimposed_img = np.clip(superimposed_img, 0, 255).astype(np.uint8)
@@ -218,4 +127,176 @@ def VizGradCAM_for_feature_map(model, img_array, interpolant=0.5, plot_results=F
         plt.show()
 
     return superimposed_img
+
+
+def display_feature_map_with_predictions_and_gradcam(model, dataset, class_names, VizGradCAM_for_feature_map,
+                                                     target_class=None):
+    class_names_list = class_names.tolist() if isinstance(class_names, np.ndarray) else class_names
+    found = False
+
+    if target_class is not None and target_class in class_names_list:
+        target_class_index = class_names_list.index(target_class)
+
+        for images, labels in dataset.unbatch().as_numpy_iterator():
+            label_index = np.argmax(labels)
+            if label_index == target_class_index:
+                image = images.astype('uint8')
+                true_class = class_names[label_index]
+                found = True
+                break
+    if not found:
+        for images, labels in dataset.shuffle(1024).take(1).unbatch().as_numpy_iterator():
+            image = images.astype('uint8')
+            true_label_index = np.argmax(labels)
+            true_class = class_names[true_label_index]
+            break
+
+    if image is not None:
+        image_expanded = np.expand_dims(image, axis=0)
+        prediction_scores = model.predict(image_expanded)
+        predicted_index = np.argmax(prediction_scores[0])
+        predicted_class = class_names[predicted_index]
+
+        gradcam_img = VizGradCAM_for_feature_map(model, image, interpolant=0.5, plot_results=False)
+
+        plt.figure(figsize=(12, 4))
+        plt.subplot(1, 2, 1)
+        plt.imshow(image)
+        plt.title(f'Faktyczna klasa: {true_class}\nPrzewidziana: {predicted_class}')
+        plt.axis('off')
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(gradcam_img)
+        plt.title(f'GradCAM')
+        plt.axis('off')
+        plt.show()
+
+        conv_layers = [layer for layer in model.layers if isinstance(layer, tf.keras.layers.Conv2D)]
+        activation_model = Model(inputs=model.input, outputs=[layer.output for layer in conv_layers])
+        feature_maps = activation_model.predict(image_expanded)
+
+        num_layers = len(feature_maps)
+        cols = 3
+        rows = (num_layers + cols - 1) // cols
+        plt.figure(figsize=(cols * 3, rows * 3))
+        for i, feature_map in enumerate(feature_maps):
+            plt.subplot(rows, cols, i + 1)
+            plt.imshow(feature_map[0, :, :, 0], cmap='viridis')
+            plt.title(conv_layers[i].name)
+            plt.axis('off')
+        plt.tight_layout()
+        plt.show()
+    else:
+        print("Nie znaleziono obrazu z podanej klasy.")
+
+
+def predict_and_gradcam(model, dataset, class_names, VizGradCAM, target_class=None):
+    class_names_list = class_names.tolist() if isinstance(class_names, np.ndarray) else class_names
+    found = False
+
+    if target_class is not None and target_class in class_names_list:
+        target_class_index = class_names_list.index(target_class)
+
+        for images, labels in dataset.unbatch().filter(lambda img, lbl: tf.argmax(lbl) == target_class_index).shuffle(1024).take(1):
+            image = images.numpy().astype('uint8')
+            label = labels.numpy()
+            found = True
+            break
+
+    if not found:
+        images, labels = next(iter(dataset.shuffle(1024).take(1).unbatch()))
+        image = images.numpy().astype('uint8')
+        label = labels.numpy()
+
+    image_expanded = np.expand_dims(image, axis=0)
+    prediction_scores = model.predict(image_expanded)
+    predicted_index = np.argmax(prediction_scores[0])
+    true_class = class_names[np.argmax(label)]
+    predicted_class = class_names[predicted_index]
+
+    gradcam_img = VizGradCAM(model, image)
+
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.imshow(image)
+    plt.title(f'Faktyczna klasa: {true_class}\nPrzewidziana: {predicted_class}')
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(image)
+    plt.imshow(gradcam_img, cmap='jet', alpha=1)
+    plt.title('GradCAM')
+    plt.axis('off')
+    plt.show()
+
+    return true_class, predicted_class
+
+
+def display_feature_map_with_predictions_and_gradcam2(model, dataset, class_names, VizGradCAM_for_feature_map,
+                                                      target_class=None):
+    class_names_list = class_names.tolist() if isinstance(class_names, np.ndarray) else class_names
+    found = False
+
+    if target_class is not None and target_class in class_names_list:
+        target_class_index = class_names_list.index(target_class)
+
+        for images, labels in dataset.unbatch().as_numpy_iterator():
+            label_index = np.argmax(labels)
+            if label_index == target_class_index:
+                image = images.astype('uint8')
+                true_class = class_names[label_index]
+                found = True
+                break
+    if not found:
+        for images, labels in dataset.shuffle(1024).take(1).unbatch().as_numpy_iterator():
+            image = images.astype('uint8')
+            true_label_index = np.argmax(labels)
+            true_class = class_names[true_label_index]
+            break
+
+    if image is not None:
+        # Przygotowanie obrazu do predykcji
+        image_expanded = np.expand_dims(image, axis=0)
+        prediction_scores = model.predict(image_expanded)
+        predicted_index = np.argmax(prediction_scores[0])
+        predicted_class = class_names[predicted_index]
+
+        # Generowanie GradCAM
+        gradcam_img = VizGradCAM_for_feature_map(model, image, interpolant=0.5, plot_results=False)
+
+        # Wyświetlanie oryginalnego obrazu i GradCAM
+        plt.figure(figsize=(12, 4))
+        plt.subplot(1, 2, 1)
+        plt.imshow(image)
+        plt.title(f'Faktyczna klasa: {true_class}\nPrzewidziana: {predicted_class}')
+        plt.axis('off')
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(gradcam_img)
+        plt.title('GradCAM')
+        plt.axis('off')
+        plt.show()
+
+        # Mapy cech dla warstw konwolucyjnych
+        conv_layers = [layer for layer in model.layers if isinstance(layer, tf.keras.layers.Conv2D)]
+        activation_model = Model(inputs=model.input, outputs=[layer.output for layer in conv_layers])
+        feature_maps = activation_model.predict(image_expanded)
+
+        # Wyświetlanie ograniczonej liczby map cech dla wszystkich filtrów
+        for layer_name, feature_map in zip([layer.name for layer in conv_layers], feature_maps):
+            n_filters = feature_map.shape[-1]
+            n_filters = min(n_filters, 64)  # Ograniczenie do maksymalnie 64 filtrów
+            n_cols = 8  # Filtry w wierszu
+            n_rows = n_filters // n_cols if n_filters % n_cols == 0 else (n_filters // n_cols) + 1
+
+            plt.figure(figsize=(n_cols * 2, n_rows * 2))
+            for i in range(n_filters):
+                ax = plt.subplot(n_rows, n_cols, i + 1)
+                plt.imshow(feature_map[0, :, :, i], cmap='viridis')
+                plt.axis('off')
+            plt.suptitle(f"Feature maps for layer {layer_name}", fontsize=16)
+            plt.subplots_adjust(wspace=0.1, hspace=0.1)
+            plt.show()
+    else:
+        print("Nie znaleziono obrazu z podanej klasy.")
 
